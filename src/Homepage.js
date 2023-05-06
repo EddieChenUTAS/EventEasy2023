215345849e0876514e068ea2d40303c35ca462ee
EChen290 <102009144+EChen290@users.noreply.github.com>
2023-05-06 18:45:10 +1000

    updates

--- src/Homepage.js
+++ src/Homepage.js
@@ -8,44 +8,44 @@ export default function Homepage() {
         <h1>Event Easy</h1>
         <p>Where amazing events get started</p>
       </div>
-
-      <img src={hobartImage} />
-
-      <section id="search-container">
-        <div id="div1">
-          <div id="div1-1">
-            <input type="text" placeholder="Search for Events, Venues" id="text-input"></input>
-            <input type="submit" value="Search" id="search-btn"></input>
+      <div id="imagebox">
+        <img src={hobartImage} id ="hobart-image"/>
+        <section id="search-container">
+          <div id="div1">
+            <div id="div1-1">
+              <input type="text" placeholder="Search for Events, Venues" id="text-input"></input>
+              <input type="submit" value="Search" id="search-btn"></input>
+            </div>
           </div>
-        </div>
-        <div id="div2">
-          <div id="divcell">
-            <p>Location</p>
-            <select id="location-select">
-              <option value="" disabled selected>
-                Select Location
-              </option>
-              <option value="0">Hobart</option>
-              <option value="1">Launceston</option>
-            </select>
+          <div id="div2">
+            <div id="divcell">
+              <p>Location</p>
+              <select id="location-select">
+                <option value="" disabled selected>
+                  Select Location
+                </option>
+                <option value="0">Hobart</option>
+                <option value="1">Launceston</option>
+              </select>
+            </div>
+            <div id="divcell">
+              <p>Category</p>
+              <select id="category-select">
+                <option value="" disabled selected>
+                  Select Category
+                </option>
+                <option value="0">Birthdat & Private</option>
+                <option value="1">Corporate Functions</option>
+                <option value="2">Weddings & Engagements</option>
+              </select>
+            </div>
+            <div id="divcell">
+              <p>Date</p>
+              <input type="date" id="date-picker" placeholder="Select Date"></input>
+            </div>
           </div>
-          <div id="divcell">
-            <p>Category</p>
-            <select id="category-select">
-              <option value="" disabled selected>
-                Select Location
-              </option>
-              <option value="0">Birthdat & Private</option>
-              <option value="1">Corporate Functions</option>
-              <option value="2">Weddings & Engagements</option>
-            </select>
-          </div>
-          <div id="divcell">
-            <p>Date</p>
-            <input type="date" id="date-picker" placeholder="Select Date"></input>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 }

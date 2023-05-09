cdedb1d9f6f4b14a965f0b566678da6067fb8944
yufengl <yufengl@utas.edu.au>
2023-05-09 13:40:22 +0800

    add material UI

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,11 +1,14 @@
+import "./index.js";
+
 import Navbar from "./navbar";
 import Footer from "./footer";
 import routes from "@/route";
 import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const Layout = () => {
   return (
-    <>
+    <Box>
       <Navbar />
       <Routes>
         {routes.map(({ path, component: Component }) => (
@@ -13,9 +16,8 @@ const Layout = () => {
         ))}
       </Routes>
       <Footer />
-    </>
+    </Box>
   );
 };
 
-
-export default Layout
+export default Layout;

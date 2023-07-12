94bd28bcbfa63014185e888d631c55e3792de3ad
EChen290 <102009144+EChen290@users.noreply.github.com>
2023-07-12 20:49:33 +1000

    changes
    revert errors

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./Homepage.css";
+import "./navbar.css";
+import "./footer.css";
+import Navbar from "./Navbar";
+import Footer from "./footer";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import logo from "./images/EventEasyLogo.png"; // Tell Webpack this JS file uses this image
+
+export default function Navbar() {
+  return (
+    <nav className="navBar">
+      <img src={logo} alt="site-logo" className="site-logo"></img>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Book an Event</a>
+        </li>
+        <li>
+          <a href="#">Past Events</a>
+        </li>
+      </ul>
+      <a href="#" className="login">
+        Login
+      </a>
+    </nav>
+  );
+}
